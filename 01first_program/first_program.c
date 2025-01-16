dcfc75cdc3277cb08c12a72f5ea5adcf6ce9ca66
nmacceus1 <nmacceus@gmail.com>
2025-01-16 16:29:07 +0000

    Implemented first program

---
+++ 01first_program/first_program.c
@@ -0,0 +1,21 @@
+// one-line comment
+// This is the first program which prints a welcoming message
+
+/*
+
+Multiline comment.
+
+ January 9 2025
+
+*/
+
+#include <stdio.h>
+
+// Create a main function
+int main() {
+    printf("Welcome\n to\n COP2271!\n");
+    printf("I\n want\n to\n code!\n");
+
+    return 0;
+    
+}
